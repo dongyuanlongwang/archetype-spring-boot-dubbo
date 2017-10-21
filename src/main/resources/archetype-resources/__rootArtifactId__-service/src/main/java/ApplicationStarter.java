3d2d464e47cbcc7695a43bcfa1133d5ed022d3d8
赵睿 <zhaoruizzb@163.com>
2017-10-21 17:53:14 +0800

    spring-boot-dubbo-starter 使用对应的骨架项目

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/ApplicationStarter.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class ApplicationStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(DataMongoApplication.class, args);
+	}
+}
